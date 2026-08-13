08c6ffa63b7d5b2eb4c6530255085a566aa99024
GerandiTri <geranditri11@gmail.com>
2026-08-13 16:59:57 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,28 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local BASE = 'https://raw.githubusercontent.com/geranditri11/TroubleMaker/refs/heads/main/Games/'
+
+local games = {
+    [15308782509] = 'Soccer-Training.lua',
+    [115681808123944] = 'Throw-A-Coin.lua',
+}   
+
+local file = games[game.PlaceId]
+
+if file then
+    task.wait(math.random())
+    
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(BASE .. file))()
+    end)
+    
+    if not success then
+        warn("Script Not Loaded !!!: " .. tostring(err))
+    else
+        print("Script Loaded !!!: " .. file)
+    end
+else
+    warn("Script Not Found (PlaceId: " .. tostring(game.PlaceId) .. ")")
+end
